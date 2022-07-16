86eebd1b1d1495c766aa62e5bf222599ebd55477
Egii <regidesoftian@gmail.com>
2022-07-17 03:05:12 +0700

    Scripts: gitsetup: add more alias config
    Signed-off-by: Egii <regidesoftian@gmail.com>

--- gitsetup.sh
+++ gitsetup.sh
@@ -9,17 +9,20 @@ msg() {
 	echo
 }
 
-msg "|| Starting git setup ||"
+msg "\\\ Starting git setup ///"
+# Git Identity
 git config --global core.editor "nano" # termux only
 git config --global user.name "Egii"
 git config --global user.email "regidesoftian@gmail.com"
 git config --global credential.helper store
 
-# Delete this if you don't want to use it
+# Git Alias
 git config --global alias.c "commit -s"
 git config --global alias.ca "cherry-pick --abort"
 git config --global alias.cr "cherry-pick --signoff"
 git config --global alias.p "push -f"
 git config --global alias.cc "cherry-pick --continue"
+git config --global alias.cs "cherry-pick --skip"
+git config --global alias.cp "cherry-pick"
 
-msg "|| Git setup done ||"
+msg "\\\ Git setup success ///"
