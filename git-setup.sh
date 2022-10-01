2f2995f0fe1f5758053cf2f75ad7d514cf124c4b
Himemori <himemori@mail.com>
2022-10-01 18:01:02 +0000

    git-setup: regemerate
    Signed-off-by: Himemori <himemori@mail.com>

--- git-setup.sh
+++ git-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Script Setup Git
-# Copyright (C) 2022 a Egii <regidesoftian@gmail.com>
+# Copyright (C) 2022 a Himemori <himemori@mail.com>
 #
 
 msg() {
@@ -10,13 +10,13 @@ msg() {
 	echo
 }
 
-msg "##===== Starting-Setup =====##"
+msg "<<--- Starting Setup --->>"
 
 ##----------------------------------------------------------##
 # Git Identity
 git config --global core.editor "nano"
-git config --global user.name "Egii"
-git config --global user.email "regidesoftian@gmail.com"
+git config --global user.name "Himemori"
+git config --global user.email "himemori@mail.com"
 git config --global credential.helper store
 
 ##----------------------------------------------------------##
@@ -30,4 +30,4 @@ git config --global alias.cc "cherry-pick --continue"
 git config --global alias.cs "cherry-pick --skip"
 git config --global alias.cp "cherry-pick"
 
-msg "##===== Setup-Succesfully =====##"
+msg "<<--- Git Setup Succesfully --->>"
